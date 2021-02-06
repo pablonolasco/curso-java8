805cb35e629dbec008caea5fff3850923e26e8fc
pablonolasco <pablo.nolasco@live.com.mx>
2021-02-06 16:39:57 -0600

    operadores

--- 02variables/src/pkg02variables/TipoDatos.java
+++ 02variables/src/pkg02variables/TipoDatos.java
@@ -38,7 +38,58 @@ public class TipoDatos {
 
     }
 
-    public void mensaje(String mensaje) {
+    public void floats() {
+        int entero=10/7;
+        float flotante=10f/7f;
+        double doble=10d/7d;
+        
+        System.out.println("Valor entero:"+entero);
+        System.out.println("Valor flotante:"+flotante);
+        System.out.println("Valor doble:"+doble);
 
+        System.out.println("Valor byte:"+Byte.MIN_VALUE);
+        System.out.println("Valor short:"+Short.MIN_VALUE);
+        System.out.println("Valor integer:"+Integer.MIN_VALUE);
+        System.out.println("Valor float:"+Float.MIN_VALUE);
+        System.out.println("Valor doble:"+Double.MIN_VALUE);
+    }
+    
+    public void conversiones(){
+        //==casting
+        int valorEntero=10/3;
+        float valorFlotante=10f/3f;
+        short valorShort=10;
+        double valorDouble=10d/3d;
+        //==casting explicito
+        byte valorByte=(byte)valorDouble;
+        //==casting implicito
+        valorEntero=valorShort;
+        System.out.println("valor:"+valorByte);
+    }
+    
+    public void constantes(){
+        final int DIAS_SEMANAS=7;
+        final int DIAS_LABORALES=5;
+        
+        System.out.println("semana:"+DIAS_SEMANAS);
+        System.out.println("laborales:"+DIAS_LABORALES);
+    }
+    
+    public void operadores(){
+        int i=12;
+        int j=10;
+        int suma=i+j;
+        int resta=i-j;
+        int multiplicacion=i*j;
+        int div=i/j;//==10 entre 12
+        int mod=i%j;//==residuo
+        i=2;
+        j=2;
+        System.out.println("Suma:"+suma);
+        System.out.println("Restas:"+resta);
+        System.out.println("Multiplicacion:"+multiplicacion);
+        System.out.println("Division:"+div);
+        System.out.println("sufijo:"+(i++));
+        System.out.println("posfijo:"+(++j));
     }
 }
