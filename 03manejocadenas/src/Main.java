07764798bab54ced327d3d6c70987e467a5a66ca
pablonolasco <pablo.nolasco@live.com.mx>
2021-02-16 21:46:49 -0600

    se creo proyecto cadenas string

---
+++ 03manejocadenas/src/Main.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author OPTIPLEX 9010
+ */
+public class Main {
+    public static void main(String[] args) {
+        
+    }
+    
+    public static void mostrarCadenas(){
+        String mensaje1;
+    }
+}
