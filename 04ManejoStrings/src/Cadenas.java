adc8e517c561a50a92f86d80d8b8f61ddb57a605
pablonolasco <pablo.nolasco@live.com.mx>
2021-03-04 20:12:49 -0600

    concatenar cadenas

---
+++ 04ManejoStrings/src/Cadenas.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author OPTIPLEX 9010
+ */
+public class Cadenas {
+    
+    public void mostrarCadenas(){
+        String mensaje1="Saludo desde clase Cadena";
+        String mensaje2="";
+        String mensaje3=null;
+        mensaje1+=" se concatena";
+        System.out.println(mensaje1);
+        
+        // concatenacion
+        String nombre="Pablo";
+        String paterno="Nolasco";
+        String materno="Castro";
+        
+        System.out.println(nombre+" "+" "+paterno+" "+materno);
+        
+    }
+    
+}
---
+++ 04ManejoStrings/src/Main.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author OPTIPLEX 9010
+ */
+public class Main {
+    public static void main(String[] args) {
+        Cadenas cadenas= new Cadenas();
+        cadenas.mostrarCadenas();
+    }
+}
