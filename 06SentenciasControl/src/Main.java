c6f0995a5628439cc6fbba63af6615912bec650e
pablonolasco <pablo.nolasco@live.com.mx>
2021-03-07 18:42:15 -0600

    sentencia de control if comparar strings

--- 06SentenciasControl/src/Main.java
+++ 06SentenciasControl/src/Main.java
@@ -9,21 +9,37 @@
  * @author Windows10
  */
 public class Main {
+
     public static void main(String[] args) {
-        int edad=0;
-        
-        String resultado=(edad>=18)?"Mayor de edad":"Menor de edad";
+        metodoIf();
+    }
+
+    public static void metodoIf() {
+        int edad = 0;
+
+        String resultado = (edad >= 18) ? "Mayor de edad" : "Menor de edad";
         System.out.println(resultado);
-        
-        if (edad >=18 ) {
+
+        if (edad >= 18) {
             System.out.println("Eresa mayor de edad");
-        }else if(edad<18 && edad>=0){
+        } else if (edad < 18 && edad >= 0) {
             System.out.println("Eres menor de edad");
-        }else{
+        } else {
             System.out.println("Edad no valida");
         }
-        
-        
-        
+
+        // Comparar strings
+        String nombre1 = "abc", nombre2 = "abc";
+        // compara referencia
+        //if (nombre1 == nombre2) {
+        if(nombre1.equalsIgnoreCase(nombre2)){
+            System.out.println("Compara valor son iguales");
+        } else {
+            System.out.println("Compara valor no son iguales");
+        }
+    }
+
+    public static void metodoSwitch() {
+
     }
 }
