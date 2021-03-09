17a97ad96e081fffc9122aac2254073876d54bbb
pablonolasco <pablo.nolasco@live.com.mx>
2021-03-08 19:56:02 -0600

    se creo metodo redondeo

--- 08ClaseMath/src/Operadores.java
+++ 08ClaseMath/src/Operadores.java
@@ -11,7 +11,7 @@
 public class Operadores {
     public static void main(String[] args) {
         // Documentacion https://docs.oracle.com/javase/8/docs/api/
-        numAleatorios();
+        metodoredondeo();
         
     }
     
@@ -22,5 +22,32 @@ public class Operadores {
         }
       
     }
+    
+    public static void numMayor(){
+        int [] num= new int[10];
+        double promedio=0;
+        for (int i = 0; i < 10; i++) {
+            num[i]=(int)Math.round(Math.random()*100);
+        }
+        
+        
+        for(int numero : num){
+            promedio+=numero;
+            System.out.println(numero);
+        }
+        System.out.println("promedio: "+(promedio/num.length));
+        System.out.println("mayor "+Math.max(10, 9));
+        System.out.println("menor "+Math.min(10, 9));
+    }
+    
+    public static void metodoredondeo(){
+        double numero=8.4;
+        
+        System.out.println("valor "+numero);
+        System.out.println("redondeo hacia arriba "+Math.ceil(numero));
+        System.out.println("redondeo hacia abajo "+Math.floor(numero));
+    }
+    
+    
     
 }
