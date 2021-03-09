df03fe5682bbc212fcf63b9bb7f92c4f6eecdd9b
pablonolasco <pablo.nolasco@live.com.mx>
2021-03-08 19:40:16 -0600

    se creo metodo para generar numeros aleatorios

--- 08ClaseMath/src/Operadores.java
+++ 08ClaseMath/src/Operadores.java
@@ -9,5 +9,18 @@
  * @author OPTIPLEX 9010
  */
 public class Operadores {
+    public static void main(String[] args) {
+        // Documentacion https://docs.oracle.com/javase/8/docs/api/
+        numAleatorios();
+        
+    }
+    
+    public static void numAleatorios(){
+        for (int i = 0; i < 10; i++) {
+            int numero = (int) Math.round(Math.random() * 100);
+            System.out.println("Numero: " + numero);
+        }
+      
+    }
     
 }
